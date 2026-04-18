da3a377f3869b5d8226a7c6898149842c06b3773
patrickMonzon <60011491+patrickMonzon@users.noreply.github.com>
2026-04-18 13:53:46 -0500

    codelab3

---
+++ 261DSMG1_17200237_T1/codelab3.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("1")
+    println("2")
+    println("3")
+
+    println("Tuesday")
+    println("Thursday")
+    println("Wednesday")
+    println("Friday")
+    println("Monday")
+}
